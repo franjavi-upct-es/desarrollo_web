a59832a05f4aa41a8a3138fcc578eb6a358d5538
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2025-05-25 17:04:56 +0200

    Frontend avanzado

--- ExpenseTracker/frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ ExpenseTracker/frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -3,6 +3,7 @@ import AuthLayout from "../../components/layouts/AuthLayout";
 import {Link, useNavigate} from 'react-router-dom';
 import Input from "../../components/Inputs/Input";
 import {validateEmail} from "../../utils/helper.js";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector"
 
 const SignUp = () => {
     const [profilePic, setProfilePic] = useState(null);
@@ -13,7 +14,30 @@ const SignUp = () => {
     const [error, setError] = useState(null);
 
     // Manejar Sign Up Form Submit
-    const handleSignUp = async (e) => {}
+    const handleSignUp = async (e) => {
+	e.preventDefault();
+
+	let profileImageUrl = "";
+
+	if (!fullName) {
+	    setError("Por favor introduce tu nombre");
+	    return;
+	};
+
+	if (!validateEmail) {
+	    setError("Por favor introduce un correo electrónico válido");
+	    return;
+	}
+
+	if (!password) {
+	    setError("Por favor introduce una contraseña válida");
+	    return;
+	};
+
+	setError("");
+
+	// SignUp API Call
+    };
 
     return (
 	<AuthLayout>
@@ -23,6 +47,9 @@ const SignUp = () => {
 		    Únete a nosotros hoy metiendo tus datos abajo.
 		</p>
 		<form onSubmit={handleSignUp}>
+
+		    <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+
 		    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 		    <Input
 			value={fullName}
@@ -39,15 +66,29 @@ const SignUp = () => {
 			placeholder="john@example.com"
 			type="text"
 		    />
-		    
-		    <Input
-			value={password}
-			onChange={({ target }) => setPassword(target.value)}
-			label="Contraseña"
-			placeholder="Min 8 caracteres"
-			type="password"
-		    />
+		    <div className="col-span-2">
+			<Input
+			    value={password}
+			    onChange={({ target }) => setPassword(target.value)}
+			    label="Contraseña"
+			    placeholder="Min 8 caracteres"
+			    type="password"
+			/>
 		    </div>
+		    </div>
+
+		    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+		    <button type="submit" className="btn-primary">
+			Regístrate
+		    </button>
+
+		    <p className="text-[13px] text-slate-800 mt-3">
+			¿Ya tienes una cuenta?{" "}
+			<Link className="font-medium text-primary underline" to="/login">
+			    Inicia Sesión
+			</Link>
+		    </p>
 		</form>
 	    </div>
 	</AuthLayout>
