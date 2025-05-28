f69171afaf7be4b1cb004e37358fb1ac1c30a045
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2025-05-28 18:38:08 +0200

    Ediciones del backend

---
+++ ExpenseTracker/backend/controllers/incomeController.js
@@ -0,0 +1,75 @@
+const xlsx = require('xlsx');
+const Income = require("../models/Income");
+
+// Add Income Source  
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const { icon, source, amount, date } = req.body;
+
+        // Validación: Comprueba si hay campos sin rellenar 
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "Hay que rellenar todos los campos" });
+        }
+
+        const newIcome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+
+        await newIcome.save();
+        res.status(200).json(newIcome);
+    } catch (error) {
+        res.status(500).json({ message: "Error en el servidor" });
+    }
+};
+
+// Get All Income Source  
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({ message: "Error en el servidor" });
+    }
+};
+
+// Delete Income Source  
+exports.deleteIncome = async (req, res) => {
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({ message: "Ingresos eliminados con éxito." });
+    } catch (error) {
+        res.status(500).json({ message: "Error en el servidor" });
+    }
+};
+
+// Download Excel  
+exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+
+        // Preparar los datos para Excel
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+        xlsx.writeFile(wv, 'detalles_ingresos.xlsx');
+        res.download('detalles_ingresos.xlsx');
+    } catch (error) {
+        res.status(500).json({ message: "Error en el servidor" });
+    }
+};
