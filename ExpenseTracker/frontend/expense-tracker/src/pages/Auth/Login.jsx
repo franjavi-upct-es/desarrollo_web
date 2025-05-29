b530c6295786388f1d81acf2831c3781bd75ed4d
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2025-05-29 10:19:24 +0200

    Nuevos archivos que conectan el backend y el frontend

--- ExpenseTracker/frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ ExpenseTracker/frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,77 +1,102 @@
 import React, { useState, useEffect } from 'react';
 import AuthLayout from "../../components/layouts/AuthLayout";
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/Input";
-import {validateEmail} from "../../utils/helper.js";
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 
 const Login = () => {
-	const [email, setEmail] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+
+    const { updateUser } = useContext(UserContext);
 
     const navigate = useNavigate();
 
     useEffect(() => {
-	    console.log("Login page mounted");
-	}, []);
-    
+        console.log("Login page mounted");
+    }, []);
+
     const handleLogin = async (e) => {
-	e.preventDefault();
+        e.preventDefault();
 
-	if (!validateEmail(email)) {
-	    setError("Introduce un correo válido.")
-	    return;
-	}
-	if (!password) {
-	    setError("Introduce la contraseña")
-	    return;
-	}
+        if (!validateEmail(email)) {
+            setError("Introduce un correo válido.")
+            return;
+        }
+        if (!password) {
+            setError("Introduce la contraseña")
+            return;
+        }
 
-	setError("");
+        setError("");
 
-	// Login API call
+        // Login API call
+        try {
+            const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+                email,
+                password,
+            });
+            const { token, user } = response.data;
+
+            if (token) {
+                localStorage.setItem("token", token);
+                updateUser(user);
+                navigate("/dashboard");
+            }
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Algo no funciona bien. Inténtalo de nuevo, por favor.");
+            }
+        }
     }
 
     return (
-	<AuthLayout>
-	    <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-		<h3 className="text-xl font-semibold text-black">Bienvenid@</h3>
-		<p className="text-xs text-slate-700 mt-[5px] mb-6">
-		    Introduce tus datos para iniciar sesión
-		</p>
+        <AuthLayout>
+            <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+                <h3 className="text-xl font-semibold text-black">Bienvenid@</h3>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6">
+                    Introduce tus datos para iniciar sesión
+                </p>
 
-		<form onSubmit={handleLogin}>
-		    <Input
-			value={email}
-			onChange={({ target }) => setEmail(target.value)}
-			label="Email"
-			placeholder="john@example.com"
-			type="text"
-		    />
-		    
-		    <Input
-			value={password}
-			onChange={({ target }) => setPassword(target.value)}
-			label="Contraseña"
-			placeholder="Min 8 caracteres"
-			type="password"
-		    />
+                <form onSubmit={handleLogin}>
+                    <Input
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)}
+                        label="Email"
+                        placeholder="john@example.com"
+                        type="text"
+                    />
 
-		    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+                    <Input
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                        label="Contraseña"
+                        placeholder="Min 8 caracteres"
+                        type="password"
+                    />
 
-		    <button type="submit" className="btn-primary">
-			Iniciar sesión
-		    </button>
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-		    <p className="text-[13px] text-slate-800 mt-3">
-			¿Aún no te has registrado?{" "}
-			<Link className="font-medium text-primary underline" to="/signup">
-			    Registrate
-			</Link>
-		    </p>
-		</form>
-	    </div>
-	</AuthLayout>
+                    <button type="submit" className="btn-primary">
+                        Iniciar sesión
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        ¿Aún no te has registrado?{" "}
+                        <Link className="font-medium text-primary underline" to="/signup">
+                            Registrate
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </AuthLayout>
     )
 }
 
--- ExpenseTracker/frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ ExpenseTracker/frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import AuthLayout from "../../components/layouts/AuthLayout";
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/Input";
-import {validateEmail} from "../../utils/helper.js";
+import { validateEmail } from "../../utils/helper.js";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector"
+import axiosInstance from '../../utils/axiosInstance.js';
+import { API_PATHS } from '../../utils/apiPaths.js';
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 
 const SignUp = () => {
     const [profilePic, setProfilePic] = useState(null);
@@ -13,85 +17,116 @@ const SignUp = () => {
 
     const [error, setError] = useState(null);
 
+    const { updateUser } = useContext(UserContext)
+
     // Manejar Sign Up Form Submit
     const handleSignUp = async (e) => {
-	e.preventDefault();
+        e.preventDefault();
 
-	let profileImageUrl = "";
+        let profileImageUrl = "";
 
-	if (!fullName) {
-	    setError("Por favor introduce tu nombre");
-	    return;
-	};
+        if (!fullName) {
+            setError("Por favor introduce tu nombre");
+            return;
+        };
 
-	if (!validateEmail) {
-	    setError("Por favor introduce un correo electrónico válido");
-	    return;
-	}
+        if (!validateEmail) {
+            setError("Por favor introduce un correo electrónico válido");
+            return;
+        }
 
-	if (!password) {
-	    setError("Por favor introduce una contraseña válida");
-	    return;
-	};
+        if (!password) {
+            setError("Por favor introduce una contraseña válida");
+            return;
+        };
 
-	setError("");
+        setError("");
 
-	// SignUp API Call
+        // SignUp API Call
+        try {
+
+            // Upload image if present 
+            if (profilePic) {
+                const imgUploadRes = await uploadImage(profilePic);
+                profileImageUrl = imgUploadRes.imageUrl || "";
+            }
+
+            const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+                fullName,
+                email,
+                password,
+                profileImageUrl,
+            });
+
+            const { token, user } = response.data;
+
+            if (token) {
+                localStorage.setItem("token", token);
+                updateUser(user);
+                navigate("/dashboard");
+            }
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Algo no funciona bien. Inténtalo de nuevo, por favor.")
+            }
+        }
     };
 
     return (
-	<AuthLayout>
-	    <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
-		<h3 className="text-xl font-semibold text-black">Crea una cuenta</h3>
-		<p className="text-xs text-slate-700 mt-[5px] mb-6">
-		    Únete a nosotros hoy metiendo tus datos abajo.
-		</p>
-		<form onSubmit={handleSignUp}>
+        <AuthLayout>
+            <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+                <h3 className="text-xl font-semibold text-black">Crea una cuenta</h3>
+                <p className="text-xs text-slate-700 mt-[5px] mb-6">
+                    Únete a nosotros hoy metiendo tus datos abajo.
+                </p>
+                <form onSubmit={handleSignUp}>
 
-		    <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+                    <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-		    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-		    <Input
-			value={fullName}
-			onChange={({ target }) => setFullName(target.value)}
-			label="Nombre completo"
-			placeholder="Luis"
-			type="text"
-		    />
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <Input
+                            value={fullName}
+                            onChange={({ target }) => setFullName(target.value)}
+                            label="Nombre completo"
+                            placeholder="Luis"
+                            type="text"
+                        />
 
-		    <Input
-			value={email}
-			onChange={({ target }) => setEmail(target.value)}
-			label="Email"
-			placeholder="john@example.com"
-			type="text"
-		    />
-		    <div className="col-span-2">
-			<Input
-			    value={password}
-			    onChange={({ target }) => setPassword(target.value)}
-			    label="Contraseña"
-			    placeholder="Min 8 caracteres"
-			    type="password"
-			/>
-		    </div>
-		    </div>
+                        <Input
+                            value={email}
+                            onChange={({ target }) => setEmail(target.value)}
+                            label="Email"
+                            placeholder="john@example.com"
+                            type="text"
+                        />
+                        <div className="col-span-2">
+                            <Input
+                                value={password}
+                                onChange={({ target }) => setPassword(target.value)}
+                                label="Contraseña"
+                                placeholder="Min 8 caracteres"
+                                type="password"
+                            />
+                        </div>
+                    </div>
 
-		    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-		    <button type="submit" className="btn-primary">
-			Regístrate
-		    </button>
+                    <button type="submit" className="btn-primary">
+                        Regístrate
+                    </button>
 
-		    <p className="text-[13px] text-slate-800 mt-3">
-			¿Ya tienes una cuenta?{" "}
-			<Link className="font-medium text-primary underline" to="/login">
-			    Inicia Sesión
-			</Link>
-		    </p>
-		</form>
-	    </div>
-	</AuthLayout>
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        ¿Ya tienes una cuenta?{" "}
+                        <Link className="font-medium text-primary underline" to="/login">
+                            Inicia Sesión
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </AuthLayout>
     )
 }
 
