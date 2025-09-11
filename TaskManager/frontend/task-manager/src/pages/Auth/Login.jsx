02d034e0a3e4b5321ef63b34514de193d911174f
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2025-09-11 16:39:52 +0200

    feat: add Input component with password visibility toggle and AuthLayout for authentication pages

--- TaskManager/frontend/task-manager/src/pages/Auth/Login.jsx
+++ TaskManager/frontend/task-manager/src/pages/Auth/Login.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react"
+import AuthLayout from "../../components/layouts/AuthLayout"
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Bienvenid@ de nuevo</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Por favor introduce tus datos para inciar sesión
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            lable="Email"
+            placeholder="john@example.com"
+            type="text"
+          />
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
